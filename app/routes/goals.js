1eb0ca73ce66f6e7561d017fbb395b82e979a017
Michael Chen <3814531+Sticksword@users.noreply.github.com>
2019-02-19 23:06:38 -0800

    update quotes and goals

--- app/routes/goals.js
+++ app/routes/goals.js
@@ -11,17 +11,14 @@ export default Route.extend({
             'title': 'Classes',
             'details': "Get A's in both CS 6460: Educational Tech and CS 8803: AI for Robots"
           }, {
-            'title': 'Business',
-            'details': 'Build a business with Michelle (?)'
-          }, {
             'title': 'Get huge & stay healthy',
-            'details': 'Play basketball & tennis. Hit the gyms hard every weekday. Bike to work. Go to sleep early fucker.'
+            'details': 'Play basketball & tennis. Gnar a bit. Hit the gyms hard every at least 4 days. Go to sleep early fucker.'
           }, {
             'title': 'Square',
             'details': 'Work towards L5, and also maintain strict schedule and become more efficient'
           }, {
             'title': 'Misc extracurriculars?',
-            'details': 'Read from time to time. Japanese (can practice with Michelle when there is rare free time). Piano.'
+            'details': 'Books. How to read and write Chinese. Piano & sightreading.'
           }, {
             'title': 'Relax and enjoy the ride',
             'details': "Don't forget to jerk off every now and then :)"
@@ -36,7 +33,7 @@ export default Route.extend({
             'details': "Either build upon Buddy or think of new things(?)"
           }, {
             'title': 'Waifu 4 laifu',
-            'details': '?'
+            'details': '???'
           }, {
             'title': 'Something',
             'details': 'Lorem Ipsum'
@@ -51,7 +48,7 @@ export default Route.extend({
             'details': "Get A's"
           }, {
             'title': 'Waifu 4 laifu',
-            'details': '?'
+            'details': '???'
           }, {
             'title': 'Something',
             'details': 'Lorem Ipsum'
